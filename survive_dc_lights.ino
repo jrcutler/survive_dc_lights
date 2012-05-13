9403db8915639e154ff7329f2efd6543167852e1
Justin R. Cutler <justin.r.cutler@gmail.com>
2012-05-12 22:55:44 -0400

    Added initial palette and multiple pattern support (with initializers).

--- survive_dc_lights.ino
+++ survive_dc_lights.ino
@@ -18,6 +18,9 @@ const int dataPin  = 2;
 const int clockPin = 3;
 #endif
 
+const uint32_t palette[] = { safety_orange, blue };
+const size_t palette_count = sizeof(palette)/sizeof(palette[0]);
+
 /*
  * Globals
  */
@@ -43,6 +46,45 @@ void fill(LPD8806 &lpd, uint32_t color, uint16_t ms)
 }
 
 /*
+ * Pattern Functions
+ */
+
+void fill_1Hz_init(LPD8806 &lpd)
+{
+  for (unsigned i = 0; i < lpd.numPixels(); ++i)
+  {
+    lpd.setPixelColor(i, palette[palette_count-1]);
+  }
+}
+
+void fill_1Hz_repeat(LPD8806 &lpd)
+{
+  // alternate colors at roughly 1 Hz
+  unsigned ms = 1000/lights.numPixels()/palette_count;
+  for (size_t i = 0; i < palette_count; ++i)
+  {
+    fill(lpd, palette[i], ms);
+  }
+}
+
+/*
+ * Pattern Definitions
+ */
+
+typedef struct
+{
+  void (*init)(LPD8806 &lpd);  
+  void (*repeat)(LPD8806 &lpd);  
+} pattern_t;
+
+const pattern_t pattern[] =
+{
+  { fill_1Hz_init, fill_1Hz_repeat },
+};
+const size_t pattern_count = sizeof(pattern)/sizeof(pattern[0]);
+size_t pattern_index = 0;
+
+/*
  * Arduino Sketch Functions
  */
 
@@ -50,15 +92,13 @@ void setup()
 {
   // initialize lights
   lights.begin();
+  pattern[pattern_index].init(lights);
   // display initial state
   lights.show();
 }
 
 void loop()
 {
-  // alternate colors at roughly 1 Hz
-  unsigned ms = 1000/lights.numPixels();
-  fill(lights, blue, ms/2);
-  fill(lights, safety_orange, ms/2);
+  pattern[pattern_index].repeat(lights);
 }
 
